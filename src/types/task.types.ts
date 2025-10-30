d5ea3e7aa53b873d88accf33aaced0d01f1956c5
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2025-10-30 01:51:02 +0100

    feat: :white_check_mark: adding taskList Reducer types: TaskListState and TaskListAction

--- src/types/task.types.ts
+++ src/types/task.types.ts
@@ -10,4 +10,16 @@ interface Task {
 
 type TaskList = Task[];
 
-export type {TaskList, Status, Priority, Task};
+// tipamos el estado
+interface TaskListState {
+  taskList: TaskList;
+}
+
+// tipamos action
+type TaskListAction =
+  | {type: "LOAD_FROM_STORAGE"; payload: TaskList}
+  | {type: "ADD"; payload: Task}
+  | {type: "DELETE"; payload: Task["id"]}
+  | {type: "EDIT"; payload: Task};
+
+export type {TaskList, Status, Priority, Task, TaskListState, TaskListAction};
